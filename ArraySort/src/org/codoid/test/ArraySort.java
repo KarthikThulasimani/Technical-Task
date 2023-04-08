c3459c352df423611b12f13d6f007cdf4aa23d15
Karthik Thulasimani <karthikthulasimani038@gmail.com>
2023-04-08 12:39:35 +0530

    First Commit

---
+++ ArraySort/src/org/codoid/test/ArraySort.java
@@ -0,0 +1,10 @@
+//16.How to sort array in java?
+package org.codoid.test;
+import java.util.Arrays;
+public class ArraySort {
+	public static void main(String[] args) {
+		int[] a= {10,20,30,50,2,-1,-10};
+		Arrays.sort(a);
+		System.out.println(Arrays.toString(a));
+	}
+       }
