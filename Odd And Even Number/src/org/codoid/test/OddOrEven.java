1eefb5a3e8808b251d38bdc2ae56f8f21b1b40e2
Karthik Thulasimani <karthikthulasimani038@gmail.com>
2023-04-08 18:43:54 +0530

    First Commit

---
+++ Odd And Even Number/src/org/codoid/test/OddOrEven.java
@@ -0,0 +1,12 @@
+//65.How to check Odd and Even Number in java.
+package org.codoid.test;
+public class OddOrEven {
+	public static void main(String[] args) {
+		int i=22;
+		if (i%2==0) {
+			System.out.println("Given Number is Even");	
+		} else {
+            System.out.println("Given Number is Odd");
+		}
+	}
+}
