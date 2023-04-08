bf67f4db371615bea2f166227bab990f48e47d5b
Karthik Thulasimani <karthikthulasimani038@gmail.com>
2023-04-08 11:50:43 +0530

    First Commit

--- Read File/src/org/codoid/test/ReadFile.java
+++ Read File/src/org/codoid/test/ReadFile.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.List;
 import org.apache.commons.io.FileUtils;
 public class ReadFile {
+	
 	public static void main(String[] args) throws IOException {
 		File f=new File("D:\\Doc\\test.txt");
 	    List<String> readLines = FileUtils.readLines(f);
@@ -16,5 +17,6 @@ public class ReadFile {
 		System.out.println(canRead);
 	    }
        }
+
 
 
