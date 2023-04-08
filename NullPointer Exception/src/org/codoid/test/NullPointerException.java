4a8a2f31585657e5fe241281911660ddae48aefd
Karthik Thulasimani <karthikthulasimani038@gmail.com>
2023-04-08 12:49:29 +0530

    First Commit

---
+++ NullPointer Exception/src/org/codoid/test/NullPointerException.java
@@ -0,0 +1,11 @@
+//20.Write a Java program to show a NullPointerException.
+package org.codoid.test;
+public class NullPointerException {
+	public static void main(String[] args) {
+		String s=null;
+		System.out.println(s.length());
+	}
+     }
+
+
+
