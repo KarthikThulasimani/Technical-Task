5f4adced2bb436ac2c9fb1f3c6f09f4bf80f7ba4
Karthik Thulasimani <karthikthulasimani038@gmail.com>
2023-04-08 11:43:05 +0530

    First Commit

---
+++ Read File/src/org/codoid/test/ReadFile.java
@@ -0,0 +1,20 @@
+//9.How to read a file in Java?
+package org.codoid.test;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import org.apache.commons.io.FileUtils;
+public class ReadFile {
+	public static void main(String[] args) throws IOException {
+		File f=new File("D:\\Doc\\test.txt");
+	    List<String> readLines = FileUtils.readLines(f);
+		for (String s : readLines) {
+			System.out.println(s);
+		}
+		
+		boolean canRead = f.canRead();
+		System.out.println(canRead);
+	    }
+       }
+
+
