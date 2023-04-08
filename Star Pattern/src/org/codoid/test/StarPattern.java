4f3a81f50a87ae9e21b8555b87c537a73b396ac9
Karthik Thulasimani <karthikthulasimani038@gmail.com>
2023-04-08 11:57:29 +0530

    First Commit

---
+++ Star Pattern/src/org/codoid/test/StarPattern.java
@@ -0,0 +1,14 @@
+//11.Write a program to generate the following output in java?
+package org.codoid.test;
+public class StarPattern {
+	public static void main(String[] args) {
+		for (int i = 0; i <=5; i++) {
+			for (int j = 0; j <=i; j++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+	  }
+     }
+
